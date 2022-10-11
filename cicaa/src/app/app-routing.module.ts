95d06c3a409e40be15cfb8c5580710b08e09752e
JOSE <usuariococ0219@gmail.com>
2022-10-11 14:36:36 -0500

    login

--- cicaa/src/app/app-routing.module.ts
+++ cicaa/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdasboardComponent } from './pages/admin/adasboard/adasboard.component';
 import { AadmistracionComponent } from './pages/admin/aadmistracion/aadmistracion.component';
+import { LoginComponent} from './pages/login/login.component'
+
+
 const routes: Routes = [
   { path: '', component: AdasboardComponent},
-  { path: 'administracion', component: AadmistracionComponent}
+  { path: 'administracion', component: AadmistracionComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
