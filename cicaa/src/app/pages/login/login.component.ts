ca5bf0ab760d726e81f14c9e3398e35f26216917
JOSE <usuariococ0219@gmail.com>
2022-11-02 17:03:01 -0500

    api

--- cicaa/src/app/pages/login/login.component.ts
+++ cicaa/src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
+import { ApiService } from '../../servicios/api/api.service';
+import { LoginI } from '../../model/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +11,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup; //////
   private isvalidemail = /\S+@\S+\.\S+/;
-  constructor(private _builder: FormBuilder) {
+  constructor(private _builder: FormBuilder, private api:ApiService) {
     this.loginForm = this._builder.group({
       username: ['', [Validators.required, Validators.pattern(this.isvalidemail)]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
-  onLogin(): void {
-    if(this.loginForm.invalid){
-      return;
-    }else{
-
-    }
- 
+  onLogin(form:LoginI){
+    this.api.loginByemail(form).subscribe(data =>{console.log(data);})
   }
 
   ngOnInit(): void {}
--- cicaa/src/app/servicios/api/api.service.ts
+++ cicaa/src/app/servicios/api/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { LoginI } from '../../model/login.interface';
+import { ResponseI } from '../../model/response.interface';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  url:string = "http://127.0.0.1:5000/"
+
+  constructor(private http:HttpClient) {}
+
+  loginByemail(form:LoginI):Observable<ResponseI>{
+    let direccion = this.url + "usuarios";
+
+    return this.http.post<ResponseI>(direccion, form);
+  }
 }
